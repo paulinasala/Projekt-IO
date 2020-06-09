ba2cde2dc1d0c5660dff582b6f913be95be2c4f6
Paulina Sala <sp44486@zut.edu.pl>
2020-06-10 01:26:04 +0200

    viemodel added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 
 namespace InteligentnyPosrednikNieruchomosci
@@ -25,12 +11,7 @@ namespace InteligentnyPosrednikNieruchomosci
         public MainWindow()
         {
             InitializeComponent();
-           
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
---
+++ RelayCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Remoting.Channels;
+using System.Windows.Input;
+
+namespace InteligentnyPosrednikNieruchomosci
+{
+    class RelayCommand : ICommand
+    {
+        private Action _action;
+
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) => _action();
+        
+    }
+}
